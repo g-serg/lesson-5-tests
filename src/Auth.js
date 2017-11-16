10c91333b03c921463809d876f5bc4e4bfb65437
g-serg <mr-sergey2004@yandex.ru>
2017-11-17 00:18:27 +0300

    tests auth done

--- src/Auth.js
+++ src/Auth.js
@@ -1,8 +1,40 @@
 import React, {Component} from 'react';
+import {authorizeUser} from './AuthorizeApi';
+import {Redirect} from 'react-router-dom';
 
 class Auth extends Component {
+  state = {
+    email: '',
+    password: '',
+    isAuthorized: false,
+    error: false
+  };
+
+  handleSubmit = () => {
+    const {email, password} = this.state;
+    const isAuthorized = authorizeUser(
+      email,
+      password
+    );
+    const error = !isAuthorized;
+
+    this.setState({isAuthorized, error});
+  };
+
   render() {
-    return null;
+    const {isAuthorized, error} = this.state;
+
+    return (
+      <div>
+        <input type="text" name="email" />
+        <input type="text" name="password" />
+        <button onClick={this.handleSubmit}>
+          Enter
+        </button>
+        {error && <p className="error">error</p>}
+        {isAuthorized && <Redirect to="/" />}
+      </div>
+    );
   }
 }
 
