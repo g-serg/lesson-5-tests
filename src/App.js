90019c27aeb1fc9a68d999778b88dbae688fa179
g-serg <mr-sergey2004@yandex.ru>
2017-11-17 01:32:22 +0300

    all tests done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import {addListener, removeListener} from './AuthorizeApi';
+import {Switch, Redirect, Route, Link} from 'react-router-dom';
+import Home from './Home';
+import Public from './Public';
+import Auth from './Auth';
+import Private from './Private';
 import './App.css';
-import {addListener, removeListener, isAuthorized} from './AuthorizeApi';
 
 class App extends Component {
   state = {
-    isAuthorized
+    isAuthorized: false
   };
 
   componentDidMount() {
@@ -20,7 +25,26 @@ class App extends Component {
   };
 
   render() {
-    return null;
+    const {isAuthorized} = this.state;
+
+    return (
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/public">Public</Link>
+          <Link to="/auth">Auth</Link>
+          <Link to="/private">Private</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/public" component={Public} />
+          <Route path="/auth" component={Auth} />
+          {isAuthorized && <Route path="/private" component={Private} />}
+          {!isAuthorized && <Redirect from="/private" to="/auth" />}
+          {!isAuthorized && <Redirect to="/" />}
+        </Switch>
+      </div>
+    );
   }
 }
 
--- src/Auth.js
+++ src/Auth.js
@@ -4,33 +4,44 @@ import {Redirect} from 'react-router-dom';
 
 class Auth extends Component {
   state = {
-    email: '',
-    password: '',
+    email: 'student@loftschool.ru',
+    password: '123',
     isAuthorized: false,
     error: false
   };
 
+  handleChange = event => {
+    const {name, value} = event.target;
+
+    this.setState({[name]: value});
+  };
+
   handleSubmit = () => {
     const {email, password} = this.state;
-    const isAuthorized = authorizeUser(
-      email,
-      password
-    );
+    const isAuthorized = authorizeUser(email, password);
     const error = !isAuthorized;
 
     this.setState({isAuthorized, error});
   };
 
   render() {
-    const {isAuthorized, error} = this.state;
+    const {isAuthorized, error, email, password} = this.state;
 
     return (
       <div>
-        <input type="text" name="email" />
-        <input type="text" name="password" />
-        <button onClick={this.handleSubmit}>
-          Enter
-        </button>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="password"
+          value={password}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleSubmit}>Enter</button>
         {error && <p className="error">error</p>}
         {isAuthorized && <Redirect to="/" />}
       </div>
